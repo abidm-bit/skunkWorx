66bea937420bbaf986c356cb6b55b1874c2cf2b4
abidm-bit <abidmres@gmail.com>
2025-04-30 13:45:21 -0400

    LC58

---
+++ src/main/java/Apr30/LC58.java
@@ -0,0 +1,20 @@
+package Apr30;
+
+import java.util.stream.Stream;
+
+public class LC58 {
+
+    static int lengthOfLastWordm1(String s) {
+        String[] s1= s.split(" ");
+        return (s1[s1.length-1].length());
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWordm1("Hello World"));                  // 5
+        System.out.println(lengthOfLastWordm1("   fly me   to   the moon  "));  // 4
+        System.out.println(lengthOfLastWordm1("luffy is still joyboy"));        // 6
+    }
+}
+
+//Stream.of(s).map(w->w.split(" ")).filter(l->l.length())
