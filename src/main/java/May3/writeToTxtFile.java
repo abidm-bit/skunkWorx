a9f95f87bbe825a5a92d31f30b03f518a787d2d3
abidm-bit <abidmres@gmail.com>
2025-05-03 22:25:54 -0400

    wrote to a text file

---
+++ src/main/java/May3/writeToTxtFile.java
@@ -0,0 +1,26 @@
+package May3;
+
+import java.io.File;
+import java.io.FileWriter;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class writeToTxtFile {
+    void main(){
+        File myObj = new File("src/main/resources/UntitledDocument.txt");
+        out.println(myObj.canRead() + " I can read this file");
+        out.println(myObj.canWrite() + " I can write to this file");
+
+        try{
+            FileWriter fileWrite = new FileWriter(myObj);
+            String input = "ratatouille, incredibles, deja vu, taking of pelham 123, limitless";
+            fileWrite.write(input);
+            fileWrite.close();
+            out.print("I wrote to this file");
+
+        } catch (Exception errorWritingToFile) {
+           out.println("I can't read or write to this this file");
+        }
+    }
+}
