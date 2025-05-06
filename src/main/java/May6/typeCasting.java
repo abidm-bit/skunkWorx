9d30d0dee892a88191fa9d54cb46df5fcca92a3b
abidm-bit <abidmres@gmail.com>
2025-05-06 14:52:08 -0400

    wide casting , narrow casting

---
+++ src/main/java/May6/typeCasting.java
@@ -0,0 +1,35 @@
+package May6;
+
+import static java.io.IO.println;
+
+class typeCasting{
+void main(){
+
+    // wide casting : converting a small datatype to a larger datatype
+    int x = 456; // prints 456
+    println(x);
+    double y = x; // prints 456.0;
+    println(y);
+
+
+    // narrowcasting: converting a larger type to a smaller type
+    double fahrenheit1 = 451.00; // prints 451.00
+    int book1 = (int)fahrenheit1; // prints 451
+    println(fahrenheit1);
+    println(book1);
+    double fahrenheit2 = 451.25; // prints 451.00
+    int book2 = (int)fahrenheit2; // prints 451
+    println(fahrenheit2);
+    println(book2);
+
+
+
+    byte z = 'a';
+    char zz = (char) z;
+    println(z); // value
+    println(zz); // a
+
+
+
+}
+}
