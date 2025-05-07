6bb5d660e35d6d0e873538517fba55420202e536
abidm-bit <abidmres@gmail.com>
2025-05-06 20:11:52 -0400

    String + Integer palindrome check

--- src/main/java/May6/twopointerPalindrome.java
+++ src/main/java/May6/twopointerPalindrome.java
@@ -16,14 +16,34 @@ class twopointerPalindrome{
         return true;
     }
 
+    // LC9. Palindrome Number
+    boolean isPalindrome(int x) {
+        String y = String.valueOf(x);
+        int i=0;
+        int e=y.length()-1;
+        while(i<=e){
+            if(y.charAt(i)!=y.charAt(e)){return false;}
+            i+=1;
+            e-=1;
+        }
+        return true;
+    }
+
 void main(){
 
     String td1 = "radar";
     String td2 = "Civic";
 
-    System.out.println(palindromeCheck(td1));
-    System.out.println(palindromeCheck(td2));
+    System.out.println(palindromeCheck(td1)); // expected true
+    System.out.println(palindromeCheck(td2)); // expected true
 
+    int x = 757;
+    int y = 456;
+    int z = 2002;
+
+    System.out.println(isPalindrome(x)); // expected true
+    System.out.println(isPalindrome(y)); // expected false
+    System.out.println(isPalindrome(z)); // expected true
 }
 
 }
