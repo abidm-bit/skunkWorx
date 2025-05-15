226f9a8528e4e7da8eb3d88d2cc4dcb9e80358e4
abidm-bit <abidmres@gmail.com>
2025-05-15 12:34:13 -0400

    median

--- src/main/java/May15/cyberChase.java
+++ src/main/java/May15/cyberChase.java
@@ -11,10 +11,26 @@ public class cyberChase {
         }
     }
 
+    static void medianElement (String []input){
+        int median = input.length/2;
+        if(input.length%2!=0){
+            System.out.println(input[median]);
+        }
+        else{
+            System.out.println(input[((median)+(median-1)/2)]);
+        }
+    }
 
     void main(){
     wantedLevel(5); // SWAT trux
     System.out.println();
     wantedLevel(6); // raptor & tanx
+    System.out.println();
+    String[]s1 = {"youtube","stack overflow","chatgpt","colleagues"};
+    String[]s2 = {"limitless","taking of pelham 123","deja vu"};
+
+    medianElement(s2);
+    medianElement(s1);
+
     }
 }
