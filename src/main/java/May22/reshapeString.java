9ff9ed094f69937c693e1d570764a3bd4052a76a
abidm-bit <abidmres@gmail.com>
2025-05-23 11:47:30 -0400

    coderpad , reshape string, regex

--- src/main/java/May22/reshapeString.java
+++ src/main/java/May22/reshapeString.java
@@ -1,6 +1,6 @@
 package May22;
 
-public class th {
+public class reshapeString {
 
 //      String line = System. lineSeparator ();
 //      String jerome = "h" + line + "p";
@@ -8,9 +8,10 @@ public class th {
     static String reshape(int n, String s){
         String nl = System.lineSeparator ();
         s=s.replaceAll("[^a-zA-Z0-9]","");
-        StringBuilder sb = new StringBuilder(s);
+        StringBuilder sb = new StringBuilder();
         for(int i=0;i<s.length();i++){
             sb.append(s.charAt(i));
+            if(i%n-1==0){sb.append(nl);}
         }
         return String.valueOf(sb);
     }
@@ -22,6 +23,10 @@ public class th {
 
         String td1 = "abc de fghij";
         String td2 = "1 23 456";
+
+        System.out.println(reshape(3,td1));
+        System.out.println();
+        System.out.println(reshape(2,td2));
 
     }
 
