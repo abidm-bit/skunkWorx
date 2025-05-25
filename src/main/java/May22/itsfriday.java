6e2c393b4260ae95280db34248358dddd819842e
abidm-bit <abidmres@gmail.com>
2025-05-25 12:16:45 -0400

    box to a case

---
+++ src/main/java/May22/itsfriday.java
@@ -0,0 +1,12 @@
+package May22;
+
+import java.util.stream.IntStream;
+
+public class itsfriday {
+
+    public static void main (String[]args){
+        IntStream.rangeClosed(0,100).forEach(System.out::println);
+
+    }
+
+}
