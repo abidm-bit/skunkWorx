1cb7a9b6e9e9264847308da75e624167a14786ff
abidm-bit <abidmres@gmail.com>
2025-06-02 15:21:04 -0400

    LC217 . revisit

---
+++ src/main/java/Nuj1/LC217.java
@@ -0,0 +1,47 @@
+package Nuj1;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.stream.Collectors;
+
+public class LC217 {
+    /*
+    * LC217 Contains Duplicates
+    * Given an integer array nums, return true if any value appears at least twice in the array,
+    * and return false if every element is distinct.
+    *
+    * */
+
+    static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> proc = new HashSet<>();
+        for(int a:nums){
+            if(proc.contains(a)){
+                return true;
+            }
+            proc.add(a);
+        }
+        return false;
+    }
+
+    static boolean containsDuplicate2(int[]nums){
+        HashSet<Integer> proc = new HashSet<>();
+        for(int a:nums){
+            proc.add(a);
+        }
+        return nums.length != proc.size();
+    }
+
+
+    public static void main (String[]args){
+
+       int [] r1 = {1,2,3,1};
+       int [] r2 ={1,2,3,4};
+        System.out.println(containsDuplicate(r1));
+        System.out.println(containsDuplicate(r2));
+
+        System.out.println(containsDuplicate2(r1));
+        System.out.println(containsDuplicate2(r2));
+
+    }
+
+}
